52fe937a738c1df1d788f2c374f1e04d6beca4e8
Colin McFadden <cmcfadden@gmail.com>
2018-12-13 15:00:46 -0500

    fix login error

--- resources/views/vendor/shibalike/IdpLogin.blade.php
+++ resources/views/vendor/shibalike/IdpLogin.blade.php
@@ -46,7 +46,7 @@
             <h2 class="title">Login to Continue</h2>
             <form action="" method="post">
                 <input type="hidden" name="_token" value="<?php echo csrf_token();?>">
-                <strong>{{ $error or "Please login below." }}</strong>
+                <strong>{{ $error ?? "Please login below." }}</strong>
                 <p>
                     <label for="username">Username</label>
                     <input type="text" name="username" id="username" />
