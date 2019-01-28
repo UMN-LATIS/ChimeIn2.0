01185ad09df81da7870acbd1e99277e3538f629e
Colin McFadden <cmcfadden@gmail.com>
2019-01-28 10:40:03 -0600

    fix logout

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -7,8 +7,8 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
         <li>
-            <a href="/logout" v-if="user.id && !user.guest_user">Log out</a>
-            <a href="/shibboleth-logout" v-else>Log in</a>
+            <a href="/shibboleth-logout" v-if="user.id && !user.guest_user">Log out</a>
+            <a href="/login" v-else>Log in</a>
         </li>
         </ul>
 
