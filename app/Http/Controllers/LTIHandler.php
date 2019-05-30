5f8a1a9800185b745b7cd920767ab3ae5bb7cff5
Colin McFadden <cmcfadden@gmail.com>
2019-05-30 15:37:28 -0500

    tweak lti

--- app/Http/Controllers/LTIHandler.php
+++ app/Http/Controllers/LTIHandler.php
@@ -27,7 +27,7 @@ class LTIHandler extends Controller
             return view("errors.emplid");    
         }
         
-        if(Auth::attempt(["emplid"=>$tool->user->sourceId])) {
+        if(Auth::attempt(["emplid"=>$tool->user->sourceId]) || Auth::attempt(["email"=>$tool->user->email])) {
             Auth::user()->lti_user_id = $tool->user->ltiUserId;
             Auth::user()->save();
         }
