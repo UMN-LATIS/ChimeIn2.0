85327b600052adb48e742878b26f287cece8b9f6
Colin McFadden <cmcfadden@gmail.com>
2019-08-27 18:31:30 -0500

    add basic docker setup.  mostly works, but kind of slow on macos

---
+++ docker/start.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "ROLE $CONTAINER_ROLE"
+
+role=${CONTAINER_ROLE:-app}
+env=${APP_ENV:-production}
+
+if [ "$env" != "local" ]; then
+    echo "Caching configuration..."
+    (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+fi
+
+if [ "$role" = "app" ]; then 
+    echo "It's an app!"
+    exec apache2-foreground
+
+elif [ "$role" = "queue" ]; then
+
+    echo "Running the queue..."
+    php /var/www/html/artisan queue:work --verbose --sleep=0.1
+
+
+elif [ "$role" = "scheduler" ]; then
+
+    while [ true ]
+    do
+      php /var/www/html/artisan schedule:run --verbose --no-interaction &
+      sleep 60
+    done
+
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
