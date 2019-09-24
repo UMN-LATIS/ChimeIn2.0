06c1e8946ea2ef38428fe7d1c1db18c3875f09a7
Colin McFadden <cmcfadden@gmail.com>
2019-09-24 11:20:40 -0500

    tweak for route model binding

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,6 +26,9 @@ class RouteServiceProvider extends ServiceProvider
         //
 
         parent::boot();
+
+        Route::model('chime', '\App\Chime');
+        Route::model('folder', '\App\Folder');
     }
 
     /**
