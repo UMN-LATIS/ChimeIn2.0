5cac82ffaade0a0da239cfb06cc8c90bde621060
Colin McFadden <cmcfadden@gmail.com>
2021-04-21 20:26:19 -0500

    add sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -41,6 +41,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
         parent::report($exception);
     }
 
@@ -58,4 +62,5 @@ class Handler extends ExceptionHandler
         }
         return parent::render($request, $exception);
     }
+    
 }
