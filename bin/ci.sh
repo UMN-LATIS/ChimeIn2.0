3e8d01addefd80f5cd3acb13f4d8a2e8444034ae
James Johnson <johnsojr@umn.edu>
2021-09-15 21:43:20 -0500

    Extracting to a separate file
    Enough with the docker-compose blahblahblah

---
+++ bin/ci.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Generate Key
+php artisan key:generate
+
+# Directory Permissions
+php artisan storage:link
+chmod -R 777 ./storage
+chmod -R 777 ./bootstrap/cache
+php artisan config:cache
+chmod -R 0755 ./vendor/laravel/dusk/bin
+
+# Upgrade Chrome Driver
+php artisan dusk:chrome-driver `/opt/google/chrome/chrome --version | cut -d " " -f3 | cut -d "." -f1`
+
+# Start Chrome Driver
+./vendor/laravel/dusk/bin/chromedriver-linux &
+
+# Migrate DB
+php artisan migrate:fresh
+
