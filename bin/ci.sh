be0351b56512228553d37e9503b35b042178e22a
James Johnson <johnsojr@gmail.com>
2021-09-16 21:15:11 +0000

    ...

--- bin/ci.sh
+++ bin/ci.sh
@@ -3,6 +3,9 @@
 # Generate Key
 php artisan key:generate
 
+# Migrate DB
+php artisan migrate:fresh
+
 # Directory Permissions
 php artisan storage:link
 chmod -R 777 ./storage
@@ -11,11 +14,10 @@ php artisan config:cache
 chmod -R 0755 ./vendor/laravel/dusk/bin
 
 # Upgrade Chrome Driver
-php artisan dusk:chrome-driver `/opt/google/chrome/chrome --version | cut -d " " -f3 | cut -d "." -f1`
+# php artisan dusk:chrome-driver `/opt/google/chrome/chrome --version | cut -d " " -f3 | cut -d "." -f1`
 
 # Start Chrome Driver
-./vendor/laravel/dusk/bin/chromedriver-linux &
+# ./vendor/laravel/dusk/bin/chromedriver-linux &
 
-# Migrate DB
-php artisan migrate:fresh
+
 
