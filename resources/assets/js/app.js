6e1f098837d4b0532a15143af7cde01b022a2c1c
James Johnson <johnsojr@umn.edu>
2021-10-01 11:06:32 -0500

    Add vue dev tools if  MIX_APP_ENV is development

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -162,3 +162,8 @@ const app = new Vue({
 }).$mount('#app');
 
 
+if (process.env.MIX_APP_ENV === 'development') {
+    Vue.config.devtools = true;
+    Vue.config.debug = true;
+    Vue.config.silent = false; 
+}
