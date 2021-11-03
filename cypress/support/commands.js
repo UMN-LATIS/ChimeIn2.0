52f0100df89eb58d885b37b8caa30759e8886df7
James Johnson <johnsojr@umn.edu>
2021-11-02 19:19:06 -0500

    Add tests for Image Heatmap question creation and response (#83)
    * disable debug bar by default
    * test: :white_check_mark: add image heatmap test with snapshot matching
    * remove unnecessary webpack import
    * remove old dusk ref

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,40 +7,57 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import 'cypress-file-upload';
+import "cypress-file-upload";
+import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
+import cypressConfig from "../../cypress.json";
 
 /**
- * Adds an artificial network delay when responding to url for the 
+ * Adds an artificial network delay when responding to url for the
  * duration of the test.
- * 
+ *
  * @param {string} url - the url path to delay
  * @param {number} delayInMs - the number of ms by which to delay the response
- * 
+ *
  * @example cy.delayResponse('/api/chime*', 1000);
- * 
+ *
  * @see https://docs.cypress.io/api/commands/intercept
  */
-Cypress.Commands.add('delayResponse', (url, delayInMs) => {
+Cypress.Commands.add("delayResponse", (url, delayInMs) => {
   cy.intercept(url, (req) => {
-    req.on('response', (res) => {
+    req.on("response", (res) => {
       return res.setDelay(delayInMs);
     });
-  }).as('delayResponse');
+  }).as("delayResponse");
+});
+
+/**
+ * Adds command for `.matchImageSnapshot()`
+ * By default, snapshots are written to `<rootDir>/cypress/snapshot`
+ *
+ * @see https://github.com/jaredpalmer/cypress-image-snapshot
+ *
+ * @example
+ * ```js
+ * // snapshot name will be the test title
+ * cy.matchImageSnapshot();
+ *
+ * // snapshot name will be the name passed in
+ * cy.matchImageSnapshot('login');
+ *
+ * // options object passed in
+ * cy.matchImageSnapshot(options);
+ *
+ * // match element snapshot
+ * cy.get('#login').matchImageSnapshot();
+ * ```
+ */
+addMatchImageSnapshotCommand(cypressConfig.matchImageSnapshot);
+
+Cypress.Commands.add("setResolution", (size) => {
+  if (Cypress._.isArray(size)) {
+    cy.viewport(size[0], size[1]);
+  } else {
+    cy.viewport(size);
+  }
 });
