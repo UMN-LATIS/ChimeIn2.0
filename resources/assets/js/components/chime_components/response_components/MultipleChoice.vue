0c293f1bf5f499c0964f7672be5ba3b22df7c58d
James Johnson <johnsojr@umn.edu>
2021-11-03 23:00:35 -0500

    WIP mult choice formatting

--- resources/assets/js/components/chime_components/response_components/MultipleChoice.vue
+++ resources/assets/js/components/chime_components/response_components/MultipleChoice.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="multiple-choice-question-options">
-    <label>
+    <label class="true-false-question-toggle">
       <input
         type="checkbox"
         v-model="isTrueFalseQuestion"
@@ -10,51 +10,113 @@
     </label>
 
     <section class="form-section">
-      <h3 class="form-section__heading">Response Choices</h3>
+      <header v-if="responses.length">
+        <h3 class="form-section__heading">
+          Response Choices
+        </h3>
+        <p class="text-muted">Check to mark response correct.</p>
+      </header>
 
-      <ol type="A" class="response-choice-list" data-cy="response-choice-list">
+      <ol class="response-choice-list" data-cy="response-choice-list">
         <draggable :list="responses">
           <li
             v-for="(response, i) in responses"
             :key="i"
-            class="response-choice-item is-draggable"
+            class="is-draggable response-choice-item"
+            :class="{ 'response-choice-item--is-correct': response.correct }"
           >
-            <label>
+            <div
+              class="response-choice-item__correct-toggle"
+              title="Mark Response Correct"
+            >
               <input type="checkbox" v-model="response.correct" />
-              <span class="visually-hidden">Correct?</span>
-            </label>
+              <label class="visually-hidden">Correct?</label>
+            </div>
+            <div class="response-choice-item__contents">
+              <label :for="`response-text-${i}`" class="visually-hidden"
+                >Response Text</label
+              >
+              <input
+                class="response-choice-item__text"
+                :id="`response-text-${i}`"
+                :name="`response-text-${i}`"
+                v-model="response.text"
+                ref="responseInput"
+                @keyup.enter="addChoice"
+              />
 
-            <label :for="`response-text_${i}`" class="visually-hidden">
-              Response Text
-            </label>
-            <input
-              :name="`response-text_${i}`"
-              :id="`response-text_${i}`"
-              v-model="response.text"
-            />
-
-            <button @click="remove(i)">
-              <i class="material-icons inline-icon">delete</i>
-            </button>
+              <button @click="remove(i)" class="response-choice-item__remove">
+                <i class="material-icons inline-icon">clear</i>
+              </button>
+            </div>
           </li>
         </draggable>
       </ol>
-      <EditResponseChoiceForm @submit="addChoice" />
+      <!-- <EditResponseChoiceForm @submit="addChoice" /> -->
+      <button class="btn btn-outline-primary" @click="addChoice">
+        Add Choice
+      </button>
     </section>
   </div>
 </template>
 
 <style scoped>
-.multiple-choice-question-options {
-  border: 1px solid red;
+label {
+  margin: 0;
 }
-
+.response-choice-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 .response-choice-item {
-  display: grid;
+  display: flex;
+  margin: 0.5rem 0;
+  align-items: center;
+}
+.response-choice-item__contents {
+  display: flex;
+  align-items: center;
+  border: 1px solid #ddd;
+  border-radius: 0.25rem;
+  flex-grow: 1;
 }
 
-button {
-  border: 1px solid #ccc;
+.response-choice-item--is-correct .response-choice-item__contents,
+.response-choice-item--is-correct .response-choice-item__contents:focus-within {
+  background: #28a745;
+  color: white;
+}
+.response-choice-item--is-correct input,
+.response-choice-item--is-correct button {
+  color: white;
+}
+
+.response-choice-item__contents:focus-within {
+  background: #eee;
+  border-color: #333;
+  outline: 0.25rem auto var(--blue);
+}
+
+.response-choice-item__correct-toggle {
+  padding: 0.5rem;
+}
+
+.response-choice-item__text {
+  border: 0;
+  flex-grow: 1;
+  padding: 0.5rem;
+  background: transparent;
+}
+
+.response-choice-item__text:focus {
+  outline: none;
+}
+
+.response-choice-item__remove {
+  display: flex;
+  background: 0;
+  border: 0;
 }
 
 .form-section {
@@ -63,31 +125,30 @@ button {
 .form-section__heading {
   font-size: 0.9rem;
   text-transform: uppercase;
-  color: #777;
+  font-weight: bold;
 }
 .is-draggable {
   cursor: move;
+}
+
+.true-false-question-toggle {
+  margin-top: 1rem;
 }
 </style>
 
 <script>
 import draggable from "vuedraggable";
-import EditResponseChoiceForm from "./EditResponseChoiceForm.vue";
 
 export default {
   props: {
-    question_responses: {
-      type: Array,
-      default: () => [],
-    },
+    question_responses: Array,
   },
   components: {
-    draggable: draggable,
-    EditResponseChoiceForm,
+    draggable,
   },
   data: function() {
     return {
-      responses: this.question_responses,
+      responses: this.question_responses || [],
       isTrueFalseQuestion: false,
     };
   },
@@ -95,10 +156,23 @@ export default {
     remove(responseIndex) {
       this.responses = this.responses.filter((_, i) => i !== responseIndex);
     },
-    addChoice({ text, isCorrect }) {
+    addChoice() {
+      // remove any choices that are blank
+      this.responses = this.responses.filter((r) => r.text !== "");
+
+      // add a new response
       this.responses.push({
-        text,
-        correct: isCorrect,
+        text: "",
+        correct: false,
+      });
+
+      // focus new choice on next tick
+      this.$nextTick(() => {
+        const lastResponseIndex = this.responses.length - 1;
+        console.log({ refs: this.$refs });
+        const ref = this.$refs.responseInput[lastResponseIndex];
+        console.log({ ref });
+        ref.focus();
       });
     },
     toggleTrueFalse: function() {
