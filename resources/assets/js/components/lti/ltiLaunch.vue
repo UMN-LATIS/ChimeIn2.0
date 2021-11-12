04a2ae3436d524a421b7a698960c06e1564787b0
James Johnson <johnsojr@umn.edu>
2021-11-11 21:10:06 -0600

    Buttons for grade passback setup

--- resources/assets/js/components/lti/ltiLaunch.vue
+++ resources/assets/js/components/lti/ltiLaunch.vue
@@ -1,22 +1,37 @@
 <template>
-    <div class="row mt-2">
-        <div class="col-12">
-            <h1>Canvas/ChimeIn Setup</h1>
-                <lti-import-selector :similar_chimes="ltiDetails.similar_chimes" v-if="ltiDetails.similar_chimes" v-on:new_chime="ltiDetails.similar_chimes = false"/>
-                <lti-setup-preferences v-else/>
-            
-
-        </div>
-    </div>
+  <div class="lti-launch-page">
+    <h1 class="lti-launch-page__heading">Canvas/ChimeIn Setup</h1>
+    <lti-import-selector
+      :similar_chimes="ltiDetails.similar_chimes"
+      v-if="ltiDetails.similar_chimes"
+      v-on:new_chime="ltiDetails.similar_chimes = false"
+    />
+    <lti-setup-preferences v-else />
+  </div>
 </template>
+
+<style scoped>
+.lti-launch-page__heading {
+  margin-top: 2rem;
+  font-weight: bold;
+}
+.lti-launch-page__heading:before {
+  display: block;
+  content: "";
+  width: 4rem;
+  height: 0.5rem;
+  background: #333;
+  margin-bottom: 0.666rem;
+}
+</style>
 
 <script>
 export default {
-    props: ["lti_details"],
-    data() {
-        return {
-            ltiDetails: this.lti_details
-        }
-    }
-}
+  props: ["lti_details"],
+  data() {
+    return {
+      ltiDetails: this.lti_details,
+    };
+  },
+};
 </script>
--- resources/assets/js/components/lti/ltiSetupPreferences.vue
+++ resources/assets/js/components/lti/ltiSetupPreferences.vue
@@ -1,75 +1,154 @@
 <template>
-    <div>
-        <p>
-            How would you like to use ChimeIn with this course? If you're not sure about these options, visit our <a href="https://umn-latis.github.io/ChimeIn2.0/canvas.html" target="_blank">help page</a>.
-        </p>
+  <div class="lti-setup-page">
+    <p>
+      How would you like to use ChimeIn with this course? If you're not sure
+      about these options, visit our
+      <a
+        href="https://umn-latis.github.io/ChimeIn2.0/canvas.html"
+        target="_blank"
+        >help page</a
+      >.
+    </p>
 
-        <h2>Grade Setup</h2>
-        <div class="form-check" v-for="gradeType in gradeTypes" :key="gradeType.id">
-          <label class="form-check-label">
-            <input type="radio" class="form-check-input" name="lti_grade_mode" :id="gradeType.id" :value="gradeType.id" :checked="gradeType.checked">
-            <b>{{ gradeType.label}}</b>
-            {{ gradeType.description }}
-          </label>
-        </div>
+    <ol class="setup-list">
+      <li class="setup-item">
+        <h2 class="setup-item__heading">
+          How should ChimeIn assignments be recorded in Canvas?
+        </h2>
 
-         <h2>Grading Calculation</h2>
-        <div class="form-check" v-for="gradeCalc in gradeCalcs" :key="gradeCalc.id">
-          <label class="form-check-label">
-            <input type="radio" class="form-check-input" name="only_correct_answers_lti" :id="gradeCalc.id" :value="gradeCalc.id" :checked="gradeCalc.checked">
-            <b>{{ gradeCalc.label}}</b>
-            {{ gradeCalc.description }}
-          </label>
+        <div class="jumbo-button-group" @update="handleUpdate">
+          <JumboButton
+            v-for="choice in gradeTypeChoices"
+            :key="choice.id"
+            :img="choice.img"
+            :title="choice.title"
+            :description="choice.description"
+            :value="choice.id"
+          />
         </div>
-        <button type="submit" class="btn btn-primary">Create Chime</button>
-    </div>
+      </li>
+    </ol>
+  </div>
 </template>
+<style scoped>
+.setup-list {
+  margin: 2rem 0;
+  padding: 0;
+  list-style: none;
+  counter-reset: setup-list;
+}
 
-<script>
-export default {
-    data() {
-        return {
-            gradeTypes: [
-                {
-                    "id": "no_grades",
-                    "label": "No grades",
-                    "description": "ChimeIn won't pass any data back to Canvas. If needed, you can still access participation data within ChimeIn",
-                    "checked": false
-                },
-                {
-                    "id": "one_grade",
-                    "label": "One Grade",
-                    "description": "This will allow you to separately track grades for each week/lecture/etc. You'll create multiple assignments in Canvas, and manage your points in Canvas.",
-                    "checked": false
-                },
-                {
-                    "id": "multiple_grades",
-                    "label": "Multiple Grades",
-                    "description": "The Canvas gradebook entry will be based on the total number of questions students respond to across all of your folders. You can still create folders in ChimeIn to organize your questions. ",
-                    "checked": true
-                }
-            ],
-            gradeCalcs: [
-                {
-                    "id": "0",
-                    "label": 'Any participation',
-                    "description": "Any response counts towards the grade in the gradebook.",
-                    "checked": true
-                },
-                {
-                    "id": "1",
-                    "label": '"Correct" answers',
-                    "description": 'Multiple Choice questions in ChimeIn can have a "correct" answer marked. When this option is selected, students responding to multiple choice questions will only recieve credit for correct answers. Any participation in other types of questions (free response, heatmap, etc) counts towards the grade.',
-                    "checked": false
-                },
-                {
-                    "id": "2",
-                    "label": 'Partial credit',
-                    "description": 'Half credit for participation, full credit for "correct" answers',
-                    "checked": false
-                }
-            ]
-        }
+.setup-list li {
+  counter-increment: setup-list;
+  padding-left: 3rem;
+  position: relative;
+}
+
+.setup-list li::before {
+  content: counter(setup-list);
+  display: flex;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  border: 2px solid #333;
+  font-weight: bold;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0.25rem;
+  left: 0;
+  background: var(--gold-light);
+}
+
+.setup-item__heading {
+    margin-bottom: 1.5rem;
+}
+
+.jumbo-button-group {
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 1rem;
+}
+
+@media (max-width: 40rem) {
+    .jumbo-button-group {
+        grid-template-columns: minmax(0, 1fr);
     }
 }
+</style>
+
+<script>
+import JumboButton from "../JumboButton.vue";
+
+export default {
+  components: {
+    JumboButton,
+  },
+  data() {
+    return {
+      gradeTypeChoice: null,
+      gradeCalcChoice: null,
+    };
+  },
+  methods: {
+    handleUpdate(x) {
+      console.log(x);
+    },
+  },
+  computed: {
+    gradeTypeChoices: () => [
+      {
+        id: "no_grades",
+        title: "No Grades",
+        description: "No participation grade will be recorded in Canvas.",
+        img: {
+          src: "/img/passback-none.svg",
+          alt:
+            "illustration that no participation will be recorded in Canvas. From left to right: Three check marks, then an arrow pointing right with an X over the arrow, then nothing to the right of the arrow.",
+        },
+      },
+      {
+        id: "one_grade",
+        title: "One Grade",
+        description:
+          "One grade column for all Chimeln assignments in Canvas, totalling all participation.",
+        img: {
+          src: "/img/passback-one.svg",
+          alt:
+            "Illustration that all assignment scores will be aggregated into one grade in Canvas. From left to right: Three checkmarks, then an arrow pointing right, then a single large star.",
+        },
+      },
+      {
+        id: "multiple_grades",
+        title: "Multiple Grades",
+        description:
+          "Separate grade column for each Chimeln assignment in Canvas",
+        img: {
+          src: "/img/passback-many.svg",
+          alt:
+            "Illustration that each Chime In Assignment will have its own grade column. From left to right: Three checkmarks, then an arrow pointing right, then 3 stars the same size as the checkmarks.",
+        },
+      },
+    ],
+    gradeCalcChoices: () => [
+      {
+        id: "0",
+        label: "Any participation",
+        description: "Any response counts towards the grade in the gradebook.",
+      },
+      {
+        id: "1",
+        label: '"Correct" answers',
+        description:
+          'Multiple Choice questions in ChimeIn can have a "correct" answer marked. When this option is selected, students responding to multiple choice questions will only recieve credit for correct answers. Any participation in other types of questions (free response, heatmap, etc) counts towards the grade.',
+      },
+      {
+        id: "2",
+        label: "Partial credit",
+        description:
+          'Half credit for participation, full credit for "correct" answers',
+      },
+    ],
+  },
+};
 </script>
