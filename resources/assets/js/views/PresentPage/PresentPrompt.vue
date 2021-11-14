4de9f9e1690b1330f58d71cfebefa7e7d8c638d1
James Johnson <johnsojr@umn.edu>
2021-11-13 18:27:54 -0600

    remove empty components

--- resources/assets/js/views/PresentPage/PresentPrompt.vue
+++ resources/assets/js/views/PresentPage/PresentPrompt.vue
@@ -3,6 +3,7 @@
     <div v-if="question" class="col">
       <h1 v-html="question.text"></h1>
       <component
+        v-if="hasSpecializedQuestionDisplay()"
         :is="question.question_info.question_type + '_display'"
         :question="question"
       />
@@ -17,22 +18,19 @@
 
 <script>
 import DisplayMultipleChoice from "../../components/MultipleChoice/MultipleChoiceDisplay.vue";
-import DisplaySlider from "../../components/SliderResponse/SliderResponseDisplay.vue";
-import DisplayTextHeatmapResponse from "../../components/TextHeatmap/TextHeatmapResponseDisplay.vue";
-import DisplayFreeResponse from "../../components/FreeResponse/FreeResponseDisplay.vue";
-import DisplayImageResponse from "../../components/ImageResponse/ImageResponseDisplay.vue";
 import DisplayHeatmapResponse from "../../components/ImageHeatmapResponse/ImageHeatmapResponseDisplay.vue";
 
 export default {
   components: {
     multiple_choice_display: DisplayMultipleChoice,
-    slider_response_display: DisplaySlider,
-    text_heatmap_response_display: DisplayTextHeatmapResponse,
-    free_response_display: DisplayFreeResponse,
-    no_response_display: DisplayFreeResponse,
-    image_response_display: DisplayImageResponse,
     heatmap_response_display: DisplayHeatmapResponse,
   },
   props: ["question", "session"],
+  methods: {
+    hasSpecializedQuestionDisplay() {
+      const questionType = this.question.question_info.question_type;
+      return ["multiple_choice", "heatmap_response"].includes(questionType);
+    },
+  },
 };
 </script>
