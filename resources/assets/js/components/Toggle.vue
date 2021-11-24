39eff1f3a473f88561ff3e7d31a12236897df4d3
James Johnson <johnsojr@umn.edu>
2021-11-24 15:20:13 -0600

    Add toggle and question type

---
+++ resources/assets/js/components/Toggle.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="toggle">
+    <input
+      class="toggle__input visually-hidden"
+      type="checkbox"
+      :checked="checked"
+      :name="name"
+      :value="value"
+      :id="name"
+      @change="handleInputChange($event)"
+    />
+    <label class="toggle__label" :for="name">
+      <span class="toggle__label-text">
+        <slot />
+      </span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    checked: {
+      type: Boolean,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+  },
+  events: ["change"],
+  methods: {
+    handleInputChange($event) {
+      this.$emit("change", $event);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.toggle {
+  --height: 1.33rem;
+  --width: 2.5rem;
+  --color: var(--black);
+  --padding: 0.75rem;
+  width: calc(var(--width) + 2 * var(--padding) + 2px);
+  padding: 0.8rem;
+}
+.toggle__label {
+  position: relative;
+  padding-top: var(--height);
+  display: block;
+  width: 100%;
+  text-align: center;
+}
+
+/* toggle outline */
+.toggle__label::before {
+  content: "";
+  display: block;
+  border: 1px solid var(--color);
+  height: var(--height);
+  width: var(--width);
+  border-radius: calc(var(--height) / 2);
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+/* toggle circle */
+.toggle__label::after {
+  --circle-diameter: calc(var(--height) - 6px);
+  content: "";
+  display: block;
+  width: var(--circle-diameter);
+  height: var(--circle-diameter);
+  border-radius: 50%;
+  background: var(--color);
+  position: absolute;
+  top: 3px;
+  left: 3px;
+  transition: 0.2s;
+}
+
+.toggle__input:checked ~ .toggle__label::before,
+.toggle__input:checked ~ .toggle__label::after,
+.toggle__input:checked ~ .toggle__label .toggle__label-text {
+  --color: var(--green);
+}
+.toggle__input:checked ~ .toggle__label::after {
+  --translation: calc(100% + 2px);
+  transform: translateX(var(--translation));
+}
+
+.toggle__label-text {
+  display: block;
+  font-size: 0.75rem;
+  margin-top: 0.4rem;
+  color: var(--color);
+  text-align: center;
+}
+</style>
--- resources/assets/js/views/FolderPage/QuestionCard.vue
+++ resources/assets/js/views/FolderPage/QuestionCard.vue
@@ -1,20 +1,20 @@
 <template>
   <Card class="question-card">
+    <footer class="question-card__footer">
+      <span class="question-card__question-type">{{ questionType }}</span>
+      <Chip color="primary">{{ totalResponses }} Responses</Chip>
+    </footer>
     <div class="flow-text question_list_text" v-html="question.text" />
 
     <template #actions>
-      <Chip color="primary">{{ totalResponses }} Responses</Chip>
-
-      <PrettyCheck
+      <Toggle
         :checked="isOpen"
-        name="isOpen"
-        data-cy="toggle-open-question"
-        class="open-question-toggle p-switch p-outline"
-        color="success"
+        :name="`question-${question.id}-isOpen`"
+        color="green"
         @change="handleToggleOpenQuestion($event)"
       >
         {{ isOpen ? "Open" : "Closed" }}
-      </PrettyCheck>
+      </Toggle>
 
       <CardActionButton icon="edit" @click="handleEditClick"
         >Edit</CardActionButton
@@ -23,30 +23,48 @@
       <CardActionButton icon="play_circle_outline" @click="handlePresentClick"
         >Present</CardActionButton
       >
+      <CardActionButton icon="clear" @click="handleDeleteClick"
+        >Delete</CardActionButton
+      >
     </template>
   </Card>
 </template>
 
 <script>
-import PrettyCheck from "pretty-checkbox-vue/check";
 import Card from "../../components/Card.vue";
 import Chip from "../../components/Chip.vue";
 import CardActionButton from "../../components/CardActionButton.vue";
+import Toggle from "../../components/Toggle.vue";
 
 export default {
   components: {
-    PrettyCheck,
     Card,
     CardActionButton,
     Chip,
+    Toggle,
   },
-  props: ["folder", "question"],
+  props: {
+    folder: {
+      type: Object,
+      required: true,
+    },
+    question: {
+      type: Object,
+      required: true,
+    },
+  },
   data: function () {
     return {
       showEdit: false,
     };
   },
   computed: {
+    questionType() {
+      return this.question.question_info.question_type
+        .split("_")
+        .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
+        .join(" ");
+    },
     totalResponses() {
       return this.question.sessions.reduce(
         (acc, session) => acc + session.responses.length,
@@ -68,14 +86,15 @@ export default {
     handleDeleteClick() {
       this.$emit("deletequestion", this.question.id);
     },
-    handleToggleOpenQuestion(setToOpen) {
+    handleToggleOpenQuestion($event) {
+      const shouldOpen = $event.target.checked;
       const chimeId = this.folder.chime_id;
       const folderId = this.folder.id;
       const questionId = this.question.id;
 
       const questionUrl = `/api/chime/${chimeId}/folder/${folderId}/question/${questionId}`;
 
-      if (setToOpen) {
+      if (shouldOpen) {
         //  open question
         axios
           .post(questionUrl)
@@ -109,5 +128,16 @@ export default {
 }
 .open-question-toggle label {
   display: block;
+}
+.question-card__question-type {
+  text-transform: uppercase;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+.question-card__footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  margin-bottom: 1rem;
 }
 </style>
