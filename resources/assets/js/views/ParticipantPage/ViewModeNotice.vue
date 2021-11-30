dcd29f8b6ecc26ef68ee8fbc55acdc248683c77a
James Johnson <johnsojr@umn.edu>
2021-11-29 20:38:46 -0600

    view mode now pink 💖

--- resources/assets/js/views/ParticipantPage/ViewModeNotice.vue
+++ resources/assets/js/views/ParticipantPage/ViewModeNotice.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="view-mode grid">
-    <div>
-      <header class="view-mode__header">
-        <i class="material-icons">preview</i>
-        <h2 class="view-mode__heading">Participant View</h2>
-      </header>
+    <header class="view-mode__header">
+      <i class="material-icons">preview</i>
+      <h2 class="view-mode__heading">Participant View</h2>
+    </header>
+    <div class="view-mode__description">
       <p>This is a preview of what your Chime participants will see.</p>
       <p>
         <a :href="canvasUrl || joinUrl">{{ canvasUrl || joinUrl }}</a>
@@ -44,13 +44,13 @@ export default {
 </script>
 <style>
 :root {
-  --color-viewmode-bg: #333;
-  --color-viewmode-heading: #ccc;
-  --color-viewmode-text: #777;
-  --color-viewmode-link: hsla(45deg, 90%, 65%, 0.9);
+  --color-viewmode-bg: #bf32a5;
+  --color-viewmode-heading: #fff;
+  --color-viewmode-text: #fff;
+  --color-viewmode-link: #fff;
 }
 .page--viewmode-participant {
-  border: 1rem solid var(--color-viewmode-bg);
+  border: 0.25rem solid var(--color-viewmode-bg);
   padding-bottom: 4rem;
 }
 </style>
@@ -65,13 +65,17 @@ export default {
   left: 0;
   z-index: 10;
   padding: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+  justify-content: space-between;
 }
 
 .view-mode__header {
   display: flex;
   align-items: center;
   color: var(--color-viewmode-heading);
-  margin-bottom: 0.5rem;
 }
 
 .view-mode__header i {
@@ -95,30 +99,28 @@ export default {
   letter-spacing: 1px;
   margin: 0;
 }
-
-.grid {
-  width: 100%;
-  display: grid;
-  grid-template-columns: 2fr 1fr;
-  gap: 1rem;
+.view-mode__description i {
+  font-size: 1rem;
+  vertical-align: text-bottom;
 }
+
+.view-mode__description a {
+  text-decoration: underline;
+}
+
 .view-mode a {
   color: var(--color-viewmode-link);
 }
 
 .view-mode p {
-  margin: 0.5em 0;
+  margin: 0.25em 0;
 }
 
-.view-mode > div:last-child {
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-}
 .view-mode .btn {
   font-size: 1rem;
   border-color: var(--color-viewmode-link);
-  color: var(--color-viewmode-link);
+  background: var(--color-viewmode-link);
+  color: var(--color-viewmode-bg);
 }
 .view-mode .btn:hover,
 .view-mode .btn:focus {
