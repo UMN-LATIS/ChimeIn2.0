f5116c3a59dbcc2dd8bf58202f459c3b0cee9151
James Johnson <johnsojr@umn.edu>
2022-02-03 17:01:09 -0600

    yarn stuff after `deploy:shared`
    - also remove commented out code

--- deploy.php
+++ deploy.php
@@ -15,12 +15,10 @@ add('shared_dirs', []);
 add('writable_dirs', []);
 
 // Servers
-
 host('dev')
     ->hostname("cla-chimein-dev.oit.umn.edu")
     ->user('swadm')
     ->stage('development')
-    // ->identityFile()
     ->set('bin/php', '/opt/rh/rh-php73/root/usr/bin/php')
     ->set('deploy_path', '/swadm/var/www/html/');
 
@@ -28,7 +26,6 @@ host('stage')
     ->hostname("cla-chimein-tst.oit.umn.edu")
     ->user('swadm')
     ->stage('stage')
-    // ->identityFile()
     ->set('bin/php', '/opt/rh/rh-php73/root/usr/bin/php')
     ->set('deploy_path', '/swadm/var/www/html/');
 
@@ -36,7 +33,6 @@ host('prod')
     ->hostname("cla-chimein-prd.oit.umn.edu")
     ->user('swadm')
     ->stage('production')
-    // ->identityFile()
     ->set('bin/php', '/opt/rh/rh-php73/root/usr/bin/php')
     ->set('deploy_path', '/swadm/var/www/html/');
 
@@ -57,24 +53,17 @@ task('fix_storage_perms', '
     sudo chgrp apache storage/logs/laravel.log
 ')->desc("Fix Apache Logs");
 
-// $result = run("scl enable rh-php56 'php -v'");
-// Tasks
-
-// desc('Restart PHP-FPM service');
-// task('php-fpm:restart', function () {
-//     // The user must have rights for restart service
-//     // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-//     run('sudo systemctl restart php-fpm.service');
-// });
-// after('deploy:symlink', 'php-fpm:restart');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
-
 before('deploy:symlink', 'artisan:migrate');
-after('deploy:update_code', 'yarn:install');
+
+// js asset generation needs to happen after
+// `deploy:shared` so that the assets can use the
+// symlinked `.env` file for variables like `MIX_APP_ENV`.
+after('deploy:shared', 'yarn:install');
 after('yarn:install', 'assets:generate');
 after('yarn:install', 'deploy:makecache');
 after('artisan:queue:restart', 'fix_storage_perms');
