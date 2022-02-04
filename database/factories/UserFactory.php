4d67d3013fcc7f47db5aee0014a172e76e5da341
James Johnson <johnsojr@umn.edu>
2022-02-04 15:12:31 -0600

    change default user factory permission to 100

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,7 +32,7 @@ class UserFactory extends Factory
         '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
       "remember_token" => str_random(10),
       "guest_user" => 0,
-      "permission_number" => 0,
+      "permission_number" => 100,
     ];
   }
 }
