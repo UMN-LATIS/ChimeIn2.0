32a4391a4d6b0b156269d180e4cba4879ad92e76
James Johnson <johnsojr@umn.edu>
2022-03-15 20:55:21 -0500

    Add TextAreaInput component

---
+++ resources/assets/js/components/TextAreaInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="textarea-input form-group">
+    <label :for="name" class="form-group__label">{{ label }}</label>
+    <div class="form-group__description">
+      <slot name="description"></slot>
+    </div>
+    <textarea
+      :id="name"
+      type="text"
+      class="form-control"
+      placeholder="Describe your image"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+    placeholder: { type: String, default: "" },
+    type: { type: String, default: "text" },
+    value: { type: String, default: "" },
+  },
+};
+</script>
+
+<style scoped>
+.form-group__label {
+  margin-bottom: 0.25rem;
+}
+.textarea-input {
+  max-width: 30rem;
+}
+
+.form-group__description {
+  font-size: 0.75rem;
+  color: #777;
+  margin-bottom: 0.5rem;
+}
+</style>
