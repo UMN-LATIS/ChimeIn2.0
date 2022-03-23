0f04f8be7a1fa63c9701265a0362d643cd871d52
James Johnson <johnsojr@umn.edu>
2022-03-22 22:02:24 -0500

    updateChimeUser api

--- cypress/integration/api/chimeUser.js
+++ cypress/integration/api/chimeUser.js
@@ -8,3 +8,40 @@ export function getChimeUsers({ chimeId }) {
     })
     .its("body");
 }
+
+export function updateChimeUser({ chimeId, userId, permissionNumber }) {
+  getChimeUsers({ chimeId })
+    .then((users) => {
+      console.log(users);
+      const user = users.find((u) => u.id === userId);
+      const updatedUser = { ...user, permission_number: permissionNumber };
+      return [...users.filter((u) => u.id !== userId), updatedUser];
+    })
+    .then((users) => {
+      console.log(users);
+      return cy.csrfToken().then((_token) => {
+        // TODO: create separate endpoint for updating a chimeUser
+        // instead of requiring updating all chime users
+        return cy.request({
+          method: "PUT",
+          url: `/api/chime/${chimeId}/users`,
+          body: {
+            users,
+            _token,
+          },
+        });
+      });
+    });
+}
+
+// export function removeChimeUser({ chimeId, userId }) {
+//   return cy.csrfToken().then((_token) => {
+//     cy.request({
+//       method: DELETE,
+//       url: `/api/chime/${chimeId}/users/${userId}`,
+//       body: {
+//         _token,
+//       },
+//     }).its("body");
+//   });
+// }
--- cypress/integration/api/chimeUsers.test.js
+++ cypress/integration/api/chimeUsers.test.js
@@ -1,5 +1,14 @@
 import { createChime } from "./chime.js";
 import * as api from "./chimeUser.js";
+// import { getCurrentUser } from "./user.js";
+
+// function addGuestUserToChime({ access_code }) {
+//   cy.logout();
+
+//   // join chime as guest
+//   return cy.visit("/join/" + access_code).then(() => getCurrentUser());
+// }
+
 describe("chimeUser api", () => {
   let testChime = null;
 
@@ -16,4 +25,53 @@ describe("chimeUser api", () => {
     api.getChimeUsers({ chimeId: testChime.id }).should("have.length", 1);
   });
 
+  it("lets presenter change a users role", () => {
+    cy.logout();
+    cy.login("student");
+    cy.visit("/join/" + testChime.access_code);
+    cy.login("faculty");
+
+    api
+      .getChimeUsers({ chimeId: testChime.id })
+      .then((users) => {
+        const user = users[1];
+        expect(user.permission_number).to.equal(100);
+        api.updateChimeUser({
+          chimeId: testChime.id,
+          userId: user.id,
+          permissionNumber: 300,
+        });
+      })
+      .then(() => {
+        api.getChimeUsers({ chimeId: testChime.id }).then((users) => {
+          const user = users[1];
+          expect(user.permission_number).to.equal(300);
+        });
+      });
+  });
+
+  it("does not allow participants/guests to change role");
+  it("gets info about current user");
+
+  // it("should remove a user from a chime", () => {
+  //   let guestUser = null;
+
+  //   addGuestUserToChime(testChime)
+  //     .then((user) => {
+  //       guestUser = user;
+
+  //       // verify that guest is in chime
+  //       cy.login("faculty");
+  //       api.getChimeUsers({ chimeId: testChime.id }).should("have.length", 2);
+  //     })
+  //     .then(() => {
+  //       api
+  //         .removeChimeUser({ chimeId: testChime.id, userId: guestUser.id })
+  //         .then(() => {
+  //           api
+  //             .getChimeUsers({ chimeId: testChime.id })
+  //             .should("have.length", 1);
+  //         });
+  //     });
+  // });
 });
