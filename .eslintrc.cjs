5339a2eae06325b6c7173642c517db1148e4d015
James Johnson <johnsojr@umn.edu>
2022-05-05 14:27:27 -0500

    update linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
     jest: true,
+    "vue/setup-compiler-macros": true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   parserOptions: {
@@ -11,7 +12,20 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["vue"],
-  rules: {},
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/attribute-hyphenation": ["error", "never"],
+    "vue/v-on-event-hyphenation": ["error", "never"],
+    "vue/component-definition-name-casing": "off",
+    "vue/component-name-in-template-casing": [
+      "warn",
+      "PascalCase",
+      {
+        registeredComponentsOnly: false,
+        ignores: ["component", "router-link", "router-view"],
+      },
+    ],
+  },
   ignorePatterns: ["**/vendor/**/*.js", "**/public/**/*.js"],
   globals: {
     axios: true,
