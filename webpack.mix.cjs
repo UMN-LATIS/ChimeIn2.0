e3b76b7d19990b02a61f30cdb38d63c00a1f663c
James Johnson <johnsojr@umn.edu>
2022-05-12 13:23:56 -0500

    first attempt

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -12,7 +12,7 @@ const mix = require("laravel-mix");
  */
 
 mix
-  .js("resources/assets/js/app.js", "public/js")
+  .ts("resources/assets/js/app.ts", "public/js")
   .vue()
   .sass("resources/assets/sass/app.scss", "public/css")
   .sourceMaps(false, "source-map");
