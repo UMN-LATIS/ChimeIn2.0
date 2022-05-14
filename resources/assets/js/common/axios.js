e77e82a569276c57cd829ae2d3cafb104d68c65c
James Johnson <johnsojr@umn.edu>
2022-05-14 09:10:09 -0500

    export axios client

--- resources/assets/js/common/axios.js
+++ resources/assets/js/common/axios.js
@@ -1,27 +1,19 @@
 import axios from "axios";
 
-export default function registerAxiosService() {
-  /**
-   * We'll load the axios HTTP library which allows us to easily issue requests
-   * to our Laravel back-end. This library automatically handles sending the
-   * CSRF token as a header based on the value of the "XSRF" token cookie.
-   */
-  window.axios = axios;
-  window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+export const axiosClient = axios;
 
-  /**
-   * Next we will register the CSRF Token as a common header with Axios so that
-   * all outgoing HTTP requests automatically have it attached. This is just
-   * a simple convenience so we don't have to attach every token manually.
-   */
+axiosClient.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-  const token = document.head.querySelector('meta[name="csrf-token"]');
+const token = document.head.querySelector('meta[name="csrf-token"]');
 
-  if (token) {
-    window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
-  } else {
-    console.error(
-      "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
-    );
-  }
+if (token) {
+  axiosClient.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
+} else {
+  console.error(
+    "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
+  );
+}
+
+export default function registerAxiosServiceGlobally() {
+  window.axios = axiosClient;
 }
