2a8211746230d37a7191a41fd27d2a7d0878d270
James Johnson <johnsojr@umn.edu>
2022-05-23 11:25:13 -0500

    change toolbar and editor options to constants

--- resources/assets/js/views/QuestionForm/TextHeatmapResponseQuestionOptions.vue
+++ resources/assets/js/views/QuestionForm/TextHeatmapResponseQuestionOptions.vue
@@ -7,64 +7,45 @@
       placeholder="Heatmap Text"
       :toolbar="toolbar"
       :options="editorOptions"
-      @update:modelValue="emitChanges"
+      @update:modelValue="
+        (updatedText) =>
+          $emit('update:question_responses', {
+            heatmap_text: updatedText,
+          })
+      "
     />
   </div>
 </template>
 
-<script>
+<script setup>
 import VEditor from "../../components/VEditor.vue";
-
-export default {
-  name: "TextHeatmapResponse",
-  components: {
-    VEditor,
+defineProps({
+  // eslint-disable-next-line vue/prop-name-casing
+  question_responses: {
+    type: Object,
+    required: true,
   },
-  props: ["question_responses"],
-  emits: ["update:question_responses"],
-  data: function () {
-    return {
-      toolbar: [
-        ["bold", "italic", "underline", "align"],
-        [
-          {
-            list: "ordered",
-          },
-          {
-            list: "bullet",
-          },
-        ],
-        [
-          {
-            script: "sub",
-          },
-          {
-            script: "super",
-          },
-          "formula",
-        ],
-        ["link"],
-      ],
-      editorOptions: {
-        bounds: ".modal-body",
-        modules: {
-          formula: true,
-          keyboard: {
-            bindings: {
-              "list autofill": {
-                prefix: /^\s{0,}(1){1,1}(\.|-|\*|\[ ?\]|\[x\])$/,
-              },
-            },
-          },
+});
+
+defineEmits(["update:question_responses"]);
+
+const toolbar = [
+  ["bold", "italic", "underline", "align"],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }, "formula"],
+  ["link"],
+];
+
+const editorOptions = {
+  bounds: ".modal-body",
+  modules: {
+    formula: true,
+    keyboard: {
+      bindings: {
+        "list autofill": {
+          prefix: /^\s{0,}(1){1,1}(\.|-|\*|\[ ?\]|\[x\])$/,
         },
       },
-    };
-  },
-  methods: {
-    emitChanges(updatedContents) {
-      this.$emit("update:question_responses", {
-        heatmap_text: updatedContents,
-      });
     },
   },
 };
