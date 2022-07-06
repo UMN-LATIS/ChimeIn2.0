3ed081720ec0be4c1f8406ba186150a73aa7466c
James Johnson <johnsojr@umn.edu>
2022-07-06 15:54:41 -0500

    Add breadcrumb items

---
+++ resources/assets/js/components/Breadcrumb.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav aria-label="breadcrumb" class="breadcrumb-wrap">
+    <ol class="breadcrumb container-fluid p-4">
+      <slot />
+    </ol>
+  </nav>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.breadcrumb-wrap {
+  border-top: 1px solid #ddd;
+}
+.breadcrumb {
+  background: none;
+  margin: 0;
+}
+</style>
