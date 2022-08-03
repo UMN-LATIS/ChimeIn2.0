7c7f19e077e5fdbd5c4c58364cb5285ee3e410f8
James Johnson <johnsojr@umn.edu>
2022-08-03 15:11:42 -0500

    vitepress updates

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,73 @@
+export default {
+  lang: "en-US",
+  title: "ChimeIn Help",
+  base: "/ChimeIn2.0/",
+  lastUpdated: true,
+  themeConfig: {
+    sidebar: [
+      {
+        text: "Getting Started",
+        items: [
+          {
+            text: "Introduction",
+            link: "/",
+          },
+          {
+            text: "Participating",
+            link: "/participate",
+          },
+          {
+            text: "Creating Questions",
+            link: "/asking-questions",
+          },
+          {
+            text: "Using with Canvas",
+            link: "/canvas",
+          },
+          {
+            text: "Cheatsheet",
+            link: "/cheatsheet",
+          },
+          {
+            text: "Accessibility",
+            link: "/accessibility",
+          },
+        ],
+      },
+      {
+        text: "Tips",
+        items: [
+          {
+            text: "Taking Attendance",
+            link: "/taking-attendance",
+          },
+          {
+            text: "Managing Chimes",
+            link: "/managing-a-chime",
+          },
+          {
+            text: "Asynchronous Usage",
+            link: "/async",
+          },
+        ],
+      },
+    ],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/UMN-LATIS/ChimeIn2.0" },
+    ],
+    editLink: {
+      pattern:
+        "https://github.com/UMN-LATIS/ChimeIn2.0/edit/develop/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2019-present Evan You",
+    },
+    // algolia: {
+    //   appId: "",
+    //   apiKey: "",
+    //   indexName: "",
+    // },
+  },
+};
