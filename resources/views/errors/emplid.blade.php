9b20c2f8f69563f7ef35985a1722c8fff2e8c6fd
James Johnson <johnsojr@umn.edu>
2022-08-25 12:18:17 -0500

    typo fixes

--- resources/views/errors/emplid.blade.php
+++ resources/views/errors/emplid.blade.php
@@ -2,6 +2,6 @@
 @section('content')
   <div class="alert alert-primary" role="alert">
     <strong>Error: </strong>a unique user identifer was not found. The "student view" option in Canvas will cause this
-    error. Please <a a href="mailto:latistecharch@umn.edu">get in touch</a> if you have more quesitons.
+    error. Please <a href="mailto:latistecharch@umn.edu">get in touch</a> if you have more questions.
   </div>
 @endsection
