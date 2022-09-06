cab932dc771ad437a4af6c29bdf4c1dd1f05ef2c
James Johnson <johnsojr@umn.edu>
2022-09-06 14:57:34 -0500

    ts fix: check that resource pk's exist before checking value

--- resources/assets/js/views/ChimePage/ChimeManagement.vue
+++ resources/assets/js/views/ChimePage/ChimeManagement.vue
@@ -45,9 +45,7 @@
             @update="handleUpdateChimeOptions"
           />
           <button
-            v-if="
-              chime.resource_link_pk > 0 || chime.lti13_resource_link_id > 0
-            "
+            v-if="isCanvasChime"
             class="btn btn-outline-success btn-sm align-items-center d-flex"
             @click="forceSyncGrades"
           >
@@ -126,23 +124,27 @@ import JoinPanel from "../../components/JoinPanel.vue";
 import { useStore } from "vuex";
 import type { Chime, ChimeOptions, User, Partial } from "../../types";
 
-interface Props {
+const props = defineProps<{
   chime: Chime;
-}
+}>();
 
-const props = defineProps<Props>();
-
-interface Emits {
+const emit = defineEmits<{
   (event: "update:chime", chimeUpdates: Partial<Chime>);
-}
-
-const emit = defineEmits<Emits>();
+}>();
 
 const users = ref<User[]>([]);
 const chimeName = ref<string>(props.chime.name);
 const store = useStore();
 
-// successful force sync
+const isCanvasChime = computed((): boolean => {
+  const { resource_link_pk, lti13_resource_link_id } = props.chime;
+
+  return (
+    (!!resource_link_pk && resource_link_pk > 0) ||
+    (!!lti13_resource_link_id && lti13_resource_link_id > 0)
+  );
+});
+
 const isForceSyncSuccessful = ref<boolean>(false);
 
 const sortedUsers = computed(() =>
