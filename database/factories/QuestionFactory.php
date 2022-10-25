a63c246459bf3414f4206953c2a6474e7b916d49
James Johnson <johnsojr@umn.edu>
2022-10-25 18:21:36 -0500

    cleanup json

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,36 +1,36 @@
 <?php
 
 namespace Database\Factories;
-use Illuminate\Support\Str;
+
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Question;
 
 class QuestionFactory extends Factory
 {
-  protected $model = \App\Question::class;
+    protected $model = Question::class;
 
-  /**
-   * Define the model's default state.
-   *
-   * @return array
-   */
-  public function definition()
-  {
-    return [
-      "text" => $this->faker->words(3, true),
-      "order" => 1,
-      "question_info" => '{
-	"question_type": "multiple_choice",
-	"question_responses": [
-            {
-                "text": "Yes",
-                "correct": false
-            },
-            {
-                "text": "No",
-                "correct": false
-            }
-        ]
-    }',
-    ];
-  }
-s}
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "text" => $this->faker->words(3, true),
+            "order" => 1,
+            "question_info" => json_encode([
+                "question_type" => "multiple_choice",
+                "question_responses" => [
+                    [
+                        "text" => "Yes",
+                        "correct" => false,
+                    ], [
+                        "text" => "No",
+                        "correct" => false,
+                    ]
+                ],
+            ]),
+        ];
+    }
+}
