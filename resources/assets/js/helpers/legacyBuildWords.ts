2dc1f45c5d404ea1d6636dd1d9096a0338563a2c
James Johnson <johnsojr@umn.edu>
2023-02-24 10:59:45 -0600

    add some comments to legacy code

--- resources/assets/js/helpers/legacyBuildWords.ts
+++ resources/assets/js/helpers/legacyBuildWords.ts
@@ -9,7 +9,7 @@ interface BuildWordsArgs {
   filterWords: string[];
 }
 
-interface LegacyWordListItem {
+interface WordListItem {
   name: string;
   stem: string;
   value: number;
@@ -19,7 +19,7 @@ export function legacyBuildWords({
   responses,
   textProcessing,
   filterWords,
-}: BuildWordsArgs): LegacyWordListItem[] {
+}: BuildWordsArgs): WordListItem[] {
   const words = responses.map((r) => r.response_info.text).join("\n ");
 
   let filteredWords;
@@ -27,49 +27,70 @@ export function legacyBuildWords({
   if (textProcessing) {
     const doc = nlp(words);
 
-    topics = doc.topics().out("array");
+    // sort so that longest is first in case topic is a substring
+    // of another topic
+    topics = doc
+      .topics()
+      .out("array")
+      .sort((a, b) => b.length - a.length);
 
+    // this will be the string of all responses without the topics
     filteredWords = words;
 
     for (let i = 0; i < topics.length; i++) {
       // remove topics words from our filtered words
+
+      // escapes any special chars from the topic that might mess up the regex
+      const escapedTopic = topics[i].replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
       filteredWords = filteredWords.replace(
-        new RegExp(
-          "\\b" + topics[i].replace(/[.*+?^${}()|[\]\\]/g, "\\$&") + "\\b",
-          "gi"
-        ),
-        ""
+        new RegExp("\\b" + escapedTopic + "\\b", "gi"),
+        "" // remove the topic
       );
+
+      // at this point, filteredWords hold all the non-topics
     }
   } else {
     filteredWords = words;
     topics = [];
   }
 
-  if (filteredWords.length == 0) {
+  // BUG: Topics
+  if (filteredWords.length == 0 && topics.length == 0) {
     return [];
   }
 
+  // words is a single string of all the responses
+  // we need to break into tokens, so that we keep things together like "New York" and "New York City" as one word
   const wordsWithoutStops = removeStopwords(
+    // keep quote stuff together, but don't grab the quotes
     filteredWords.match(/"(.*?)"|\w+/g)
   );
 
+  // combine the topics and the our nonTopic words into one array
+  // (non topic words could also be "Quoted String" ..)
   const finalizedWords = wordsWithoutStops
     .concat(topics)
+    // remove user defined filter words
     .filter((w) => !filterWords.includes(w));
 
-  const groups = finalizedWords.reduce((acc: any[], w) => {
+  const groups: WordListItem[] = finalizedWords.reduce((acc: any[], w) => {
+    // don't include single letter words or numbers
     if (w.length < 2) {
       return acc;
     }
-    const stem = stemmer(w.toLowerCase().replace(/"/g, ""));
-    const i = acc.findIndex((e) => e.stem === stem);
 
-    if (i > -1) {
-      acc[i].value += 1;
+    const unquotedWord = w.replace(/"/g, "");
+
+    const normalizedWord = unquotedWord.toLowerCase();
+    const stem = stemmer(normalizedWord);
+
+    const indexOfStem = acc.findIndex((e) => e.stem === stem);
+
+    if (indexOfStem > -1) {
+      acc[indexOfStem].value += 1;
     } else {
       acc.push({
-        name: w.replace(/"/g, ""),
+        name: unquotedWord,
         value: 1,
         stem: stem,
       });
@@ -78,9 +99,11 @@ export function legacyBuildWords({
     return acc;
   }, []);
 
+  // remove only the most important word. So sort them by count
   const sortedArray = groups.sort((a, b) => {
     return b.value - a.value;
   });
 
+  // only return the top 200 words
   return sortedArray.slice(0, 200);
 }
