998b15e13e681d40ed42556610ee99fca86caad8
James Johnson <johnsojr@umn.edu>
2023-03-07 10:26:03 -0600

    re-enable delayResponse

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,24 +12,24 @@
 // import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
 // import cypressConfig from "../../cypress.config";
 
-// /**
-//  * Adds an artificial network delay when responding to url for the
-//  * duration of the test.
-//  *
-//  * @param {string} url - the url path to delay
-//  * @param {number} delayInMs - the number of ms by which to delay the response
-//  *
-//  * @example cy.delayResponse('/api/chime*', 1000);
-//  *
-//  * @see https://docs.cypress.io/api/commands/intercept
-//  */
-// Cypress.Commands.add("delayResponse", (url, delayInMs) => {
-//   cy.intercept(url, (req) => {
-//     req.on("response", (res) => {
-//       return res.setDelay(delayInMs);
-//     });
-//   }).as("delayResponse");
-// });
+/**
+ * Adds an artificial network delay when responding to url for the
+ * duration of the test.
+ *
+ * @param {string} url - the url path to delay
+ * @param {number} delayInMs - the number of ms by which to delay the response
+ *
+ * @example cy.delayResponse('/api/chime*', 1000);
+ *
+ * @see https://docs.cypress.io/api/commands/intercept
+ */
+Cypress.Commands.add("delayResponse", (url, delayInMs) => {
+  cy.intercept(url, (req) => {
+    req.on("response", (res) => {
+      return res.setDelay(delayInMs);
+    });
+  }).as("delayResponse");
+});
 
 // /**
 //  * Adds command for `.matchImageSnapshot()`
