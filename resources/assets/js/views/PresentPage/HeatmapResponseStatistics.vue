414447f143d28b5c8c0509b1be7fd18cbeb677a8
James Johnson <johnsojr@umn.edu>
2023-09-15 10:12:13 -0500

    add a pin to each location

--- resources/assets/js/views/PresentPage/HeatmapResponseStatistics.vue
+++ resources/assets/js/views/PresentPage/HeatmapResponseStatistics.vue
@@ -2,6 +2,35 @@
   <div class="col-sm-12">
     <div class="overlay-container">
       <canvas id="simpleheat" ref="targetCanvas"></canvas>
+      <ul class="pin-container">
+        <li
+          v-for="response in responses"
+          :key="response.id"
+          :style="getPinDropStyle(response)"
+          class="pin"
+        >
+          <span class="sr-only">
+            {{ response.response_info.image_coordinates.coordinate_x }},
+            {{ response.response_info.image_coordinates.coordinate_y }}
+          </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            class="pin-icon"
+          >
+            <path
+              fill="currentColor"
+              stroke="white"
+              stroke-width="1"
+              d="M16 2A11.013 11.013 0 0 0 5 13a10.889 10.889 0 0 0 2.216 6.6s.3.395.349.452L16 30l8.439-9.953c.044-.053.345-.447.345-.447l.001-.003A10.885 10.885 0 0 0 27 13A11.013 11.013 0 0 0 16 2Zm0 15a4 4 0 1 1 4-4a4.005 4.005 0 0 1-4 4Z"
+            />
+            <circle cx="16" cy="13" r="4" fill="white" />
+          </svg>
+        </li>
+      </ul>
+
       <img
         ref="targetImage"
         data-cy="image-heatmap-original"
@@ -18,7 +47,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref, watch } from "vue";
+import { StyleValue, computed, onMounted, onUnmounted, ref, watch } from "vue";
 import simpleheat from "simpleheat";
 import { ImageHeatmapQuestion, ImageHeatmapResponse } from "@/types";
 
@@ -31,6 +60,43 @@ const props = defineProps<Props>();
 const targetImage = ref<HTMLImageElement | null>(null);
 const targetCanvas = ref<HTMLCanvasElement | null>(null);
 
+interface Point {
+  x: number;
+  y: number;
+}
+
+const scaleX = computed(() => {
+  if (!targetImage.value) {
+    return 1;
+  }
+  return targetImage.value.clientWidth / targetImage.value.naturalWidth;
+});
+const scaleY = computed(() => {
+  if (!targetImage.value) {
+    return 1;
+  }
+  return targetImage.value.clientHeight / targetImage.value.naturalHeight;
+});
+
+function getScaledImageCoords(p: Point): Point {
+  return {
+    x: p.x * scaleX.value,
+    y: p.y * scaleY.value,
+  };
+}
+
+function getPinDropStyle(response: ImageHeatmapResponse): StyleValue {
+  const coords = getScaledImageCoords({
+    x: response.response_info.image_coordinates.coordinate_x,
+    y: response.response_info.image_coordinates.coordinate_y,
+  });
+  return {
+    position: "absolute",
+    top: coords.y + "px",
+    left: coords.x + "px",
+  };
+}
+
 function renderHeatMap() {
   if (!targetCanvas.value || !targetImage.value) {
     return;
@@ -38,23 +104,19 @@ function renderHeatMap() {
 
   targetCanvas.value.width = targetImage.value.clientWidth;
   targetCanvas.value.height = targetImage.value.clientHeight;
-  const scaleFactorX =
-    targetImage.value.clientWidth / targetImage.value.naturalWidth;
-  const scaleFactorY =
-    targetImage.value.clientHeight / targetImage.value.naturalHeight;
 
   const data = props.responses.map((r) => {
     return [
-      r.response_info.image_coordinates.coordinate_x * scaleFactorX,
-      r.response_info.image_coordinates.coordinate_y * scaleFactorY,
+      r.response_info.image_coordinates.coordinate_x * scaleX.value,
+      r.response_info.image_coordinates.coordinate_y * scaleY.value,
       0.1,
     ];
   });
   simpleheat("simpleheat")
     .data(data)
     .radius(
-      50 * Math.min(scaleFactorX, scaleFactorY),
-      20 * Math.min(scaleFactorX, scaleFactorY)
+      50 * Math.min(scaleX.value, scaleY.value),
+      20 * Math.min(scaleX.value, scaleY.value)
     )
     .draw();
 }
@@ -81,5 +143,33 @@ canvas {
   right: 0;
   bottom: 0;
   left: 0;
+}
+
+.pin-container {
+  position: absolute;
+  inset: 0;
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+}
+
+.pin {
+  padding: 0;
+  margin: 0;
+  width: 0.5rem;
+  height: 0.25rem;
+  border-radius: 50%;
+  background: hsla(0, 0%, 0%, 0.25);
+  backdrop-filter: blur(0.5rem);
+}
+
+.pin-icon {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+  color: #007bff;
+  width: 1.5rem;
+  height: 1.5rem;
 }
 </style>
