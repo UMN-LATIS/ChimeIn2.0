a3bbbf13a663a324baa65870b334b68c472cfe99
James Johnson <johnsojr@umn.edu>
2023-09-19 12:34:46 -0500

    fix for triggering native events on range input

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,30 @@ Cypress.Commands.add("delayResponse", (url, delayInMs) => {
     });
   }).as("delayResponse");
 });
+
+/**
+ * Fix for triggering native events on a range input
+ * https://github.com/cypress-io/cypress/issues/1570#issuecomment-891244917
+ */
+
+Cypress.Commands.add(
+  "setSliderValue",
+  { prevSubject: "element" },
+  (subject, value) => {
+    const element = subject[0];
+
+    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+      window.HTMLInputElement.prototype,
+      "value",
+    )?.set;
+
+    nativeInputValueSetter?.call(element, value);
+    element.dispatchEvent(new Event("input", { bubbles: true }));
+  },
+);
+
+// declare namespace Cypress {
+//     interface Chainable {
+//         setSliderValue(value: number): Chainable<void>
+//     }
+// }
