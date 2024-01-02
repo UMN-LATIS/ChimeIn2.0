660a7f026c55715751344e28764a46a4a4ad358e
James Johnson <johnsojr@umn.edu>
2024-01-02 16:11:27 -0600

    Update LTI Setup Page with defaults and new graphics (#793)
    * as mult grad passback and any participation as default
    
    change to composition api while we're at it
    
    * update setup graphics
    
    - changed passback illustration for grade column choices to better reflect what will appear in the gradebook (multiple columns per student).
    - change participation grading illustration to only show color on the option that changes - how incorrect answers are graded
    - reorder so that the defaults are first. it looks strange to have one default last and another first.

--- resources/assets/js/components/JumboRadio.vue
+++ resources/assets/js/components/JumboRadio.vue
@@ -6,48 +6,41 @@
       :name="name"
       :value="value"
       :checked="isActive"
-      @change="$emit('change', $event.target.value)"
+      @change="$emit('change', ($event.target as HTMLInputElement).value)"
     />
     <img class="jumbo-radio__img" :src="img.src" :alt="img.alt" />
 
     <h3 class="jumbo-radio__title">{{ title }}</h3>
+    <h4 v-if="subtitle" class="jumbo-radio__subtitle">{{ subtitle }}</h4>
     <p class="jumbo-radio__description">
       {{ description }}
     </p>
   </label>
 </template>
 
-<script>
-export default {
-  name: "JumboRadio",
-  props: {
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
     img: {
-      type: Object,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    value: {
-      type: String,
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      default: false,
-    },
+      src: string;
+      alt: string;
+    };
+    title: string;
+    subtitle?: string;
+    description: string;
+    name: string;
+    value: string;
+    isActive?: boolean;
+  }>(),
+  {
+    subtitle: "",
+    isActive: false,
   },
-  emits: ["change"],
-};
+);
+
+defineEmits<{
+  (eventName: "change", value: string);
+}>();
 </script>
 
 <style lang="scss">
@@ -58,6 +51,7 @@ export default {
   background: #f4f4f4;
   transition: all 0.1s ease-out;
   text-align: center;
+  cursor: pointer;
 }
 
 @media (max-width: 60rem) {
@@ -83,15 +77,24 @@ export default {
   }
 }
 
+.jumbo-radio:not(.jumbo-radio--is-active) {
+  opacity: 0.66;
+}
+
 .jumbo-radio__img {
   display: block;
   margin: 0 auto 2rem;
-  filter: grayscale(1) opacity(0.666);
+  filter: grayscale(1);
   max-width: 100%;
 }
 
 .jumbo-radio__title {
   font-size: 1.25rem;
+}
+.jumbo-radio__subtitle {
+  font-size: 0.75rem;
+  color: #777;
+  margin: 0 0 1rem;
 }
 
 .jumbo-radio__description {
