e60f2f625b3600085a90bef6d899d6085008377e
James Johnson <johnsojr@umn.edu>
2024-07-18 16:16:12 -0500

    add label to axes

--- resources/assets/js/components/NumericResponse/BarChart.vue
+++ resources/assets/js/components/NumericResponse/BarChart.vue
@@ -10,36 +10,43 @@
   </div>
 </template>
 <script setup lang="ts">
+import { computed } from "vue";
 import { GChart } from "vue-google-charts";
 import { GoogleChartOptions } from "vue-google-charts/dist/types";
 
-defineProps<{
+const props = defineProps<{
   data: [label: string, value: number][];
   itemLabel: string;
   valueLabel: string;
 }>();
 
-const options: GoogleChartOptions = {
-  // height: "100%",
-  animation: {
-    duration: 1000,
-    easing: "out",
-    startup: true,
-  },
-  legend: {
-    position: "none",
-  },
-  chartArea: {
-    top: 30,
-    left: 50,
-    width: "100%",
-  },
-  vAxis: {
-    baseline: 0,
-  },
-  tooltip: {
-    isHtml: false,
-  },
-};
+const options = computed(
+  (): GoogleChartOptions => ({
+    // height: "100%",
+    animation: {
+      duration: 1000,
+      easing: "out",
+      startup: true,
+    },
+    legend: {
+      position: "none",
+    },
+    chartArea: {
+      top: 30,
+      left: 50,
+      width: "100%",
+    },
+    vAxis: {
+      baseline: 0,
+      title: "Responses",
+    },
+    hAxis: {
+      title: props.valueLabel,
+    },
+    tooltip: {
+      isHtml: false,
+    },
+  })
+);
 </script>
 <style scoped></style>
