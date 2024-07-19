1ed4d24a3360c442f06c35966ba9a0a4ecaa580a
James Johnson <johnsojr@umn.edu>
2024-07-19 17:04:25 -0500

    add range chart icon

---
+++ resources/assets/js/icons/IconChartRange.vue
@@ -0,0 +1,52 @@
+<template>
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 256 256"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="69.3092" cy="53.31" r="20.6819" fill="currentColor" />
+    <circle cx="69.3092" cy="148.74" r="20.6819" fill="currentColor" />
+    <rect
+      x="58.968"
+      y="54.3996"
+      width="20.6819"
+      height="94.3404"
+      rx="10.3409"
+      fill="currentColor"
+    />
+    <circle cx="128" cy="104.469" r="20.6819" fill="currentColor" />
+    <circle cx="128" cy="188.276" r="20.6819" fill="currentColor" />
+    <rect
+      x="117.659"
+      y="101.025"
+      width="20.6819"
+      height="87.3507"
+      rx="10.3409"
+      fill="currentColor"
+    />
+    <circle cx="186.691" cy="156.496" r="20.6819" fill="currentColor" />
+    <circle cx="186.691" cy="46.5086" r="20.6819" fill="currentColor" />
+    <rect
+      x="176.35"
+      y="40.6864"
+      width="20.6819"
+      height="121.86"
+      rx="10.3409"
+      fill="currentColor"
+    />
+    <rect
+      x="25.2252"
+      y="217.916"
+      width="205.549"
+      height="8"
+      rx="4"
+      fill="currentColor"
+    />
+  </svg>
+</template>
+<script lang="ts">
+export default {
+  name: "IconChartRange",
+};
+</script>
