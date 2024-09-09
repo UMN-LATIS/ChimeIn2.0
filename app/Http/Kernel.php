f5ea967e64f762ffe84e450c004a85bdaeedcefe
James Johnson <johnsojr@umn.edu>
2024-09-09 16:31:42 -0500

    add middleware to prevent json payloads over 10kb (#920)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel {
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'shibinjection' => \App\Http\Middleware\ShibInjection::class
+        'shibinjection' => \App\Http\Middleware\ShibInjection::class,
+        'limit.json.size'  => \App\Http\Middleware\LimitJsonSize::class,
     ];
 }
