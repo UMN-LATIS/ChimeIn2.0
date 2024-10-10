4f70d0581c79ee0c7ee990fd0cf1bcef29faf9af
James Johnson <johnsojr@umn.edu>
2024-10-10 13:15:27 -0500

    install nova scaffolding
    `php artisan nova:install`

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
         StudentAffairsUwm\Shibboleth\ShibbolethServiceProvider::class,
