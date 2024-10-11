45274d961c9aecee9dc517159bb22cc7369de412
James Johnson <johnsojr@umn.edu>
2024-10-11 15:54:30 -0500

    fix nova deployment resourceLinks relationship

--- app/LTI13Deployment.php
+++ app/LTI13Deployment.php
@@ -2,14 +2,19 @@
 
 namespace App;
 
-
 use Illuminate\Database\Eloquent\Model;
 
 class LTI13Deployment extends Model
 {
     protected $table = 'lti13_deployments';
-    public function issuer() {
+
+    public function issuer()
+    {
         return $this->belongsTo(LTI13Issuer::class);
     }
 
+    public function resourceLinks()
+    {
+        return $this->hasMany(LTI13ResourceLink::class, 'deployment_id');
+    }
 }
