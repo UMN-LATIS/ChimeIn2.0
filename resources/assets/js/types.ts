2c4fcd34ccffef74c1b95f58e2324a9329de0135
James Johnson <johnsojr@umn.edu>
2025-02-17 13:12:24 -0600

    correctly indicate that x_axis_label might be undefined

--- resources/assets/js/types.ts
+++ resources/assets/js/types.ts
@@ -219,7 +219,7 @@ export interface NumericResponseQuestionInfo extends QuestionInfo {
   question_type: "numeric_response";
   question_responses: {
     chart_type: NumericChartType;
-    x_axis_label: string;
+    x_axis_label?: string;
     y_axis_label?: string; // only for scatter
   };
 }
