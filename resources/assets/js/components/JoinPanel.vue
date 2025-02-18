09d5a200e62a50b37f7cc53bcb3d457b0983851a
James Johnson <johnsojr@umn.edu>
2025-02-18 13:15:17 -0600

    add QR code to join panel

--- resources/assets/js/components/JoinPanel.vue
+++ resources/assets/js/components/JoinPanel.vue
@@ -14,101 +14,88 @@
       </h2>
     </header>
 
-    <div v-if="isCanvasChime" class="join-panel__instructions">
-      <p>Visit your course in Canvas:</p>
-      <a :href="canvasUrl.origin" data-cy="canvas-host" target="_blank">{{
-        canvasUrl.host
-      }}</a>
+    <template v-if="isCanvasChime">
+      <QRCode v-if="canvasUrl" :url="canvasUrl.href" />
+      <p class="tw-text-center !tw-mb-6">
+        Find your assignment in Canvas
+        <a data-cy="chime-host" :href="canvasUrl?.host" class="tw-font-mono">{{
+          canvasUrl?.host
+        }}</a
+        >.
+      </p>
 
-      <details>
-        <summary>Join Instructions for Ungraded Guests</summary>
-        <ol>
-          <li>
-            Go to:
-            <a data-cy="chime-host" :href="joinUrl">{{ joinUrl }}</a>
-          </li>
-          <li>
-            Or: visit
+      <details class="tw-bg-black/5 tw-rounded">
+        <summary
+          class="tw-bg-black/5 tw-px-2 tw-py-1 tw-flex tw-justify-center tw-rounded"
+        >
+          Instructions for Ungraded Guest
+        </summary>
+        <div class="join-panel__instructions">
+          <p>
+            Guests may also join this Chime. They will NOT recieve grades in
+            Canvas if they join with the code below.
+          </p>
+          <p class="tw-text-center">
+            Visit
             <a data-cy="chime-host" :href="joinUrl">{{ location.host }}</a> and
-            enter:
-            <b class="join-panel__access-code" data-cy="access-code">{{
-              toHyphenatedCode(chime.access_code)
-            }}</b>
-          </li>
-        </ol>
+            enter code
+            <b
+              class="tw-font-mono tw-flex tw-justify-center tw-text-2xl"
+              data-cy="access-code"
+              >{{ toHyphenatedCode(chime.access_code) }}</b
+            >
+          </p>
+        </div>
       </details>
-    </div>
+    </template>
 
-    <div v-else class="join-panel__instructions">
-      <ol v-if="includeFullUrl">
-        <li>
-          Go to:
-          <a data-cy="chime-host" :href="joinUrl">{{ joinUrl }}</a>
-        </li>
-        <li>
-          Or: visit
+    <template v-else>
+      <QRCode :url="joinUrl" />
+
+      <div class="join-panel__instructions">
+        <p class="tw-text-center">
+          Visit
           <a data-cy="chime-host" :href="joinUrl">{{ location.host }}</a> and
-          enter
-          <b class="join-panel__access-code" data-cy="access-code">{{
-            toHyphenatedCode(chime.access_code)
-          }}</b>
-        </li>
-      </ol>
-      <ol v-else>
-        <li>
-          Go to:
-          <a data-cy="chime-host" :href="location.origin">{{
-            location.host
-          }}</a>
-        </li>
-        <li>
-          Enter Code
-          <b class="join-panel__access-code" data-cy="access-code">{{
-            toHyphenatedCode(chime.access_code)
-          }}</b>
-        </li>
-      </ol>
-    </div>
+          enter code
+          <b
+            class="tw-font-mono tw-flex tw-justify-center tw-text-2xl"
+            data-cy="access-code"
+            >{{ toHyphenatedCode(chime.access_code) }}</b
+          >
+        </p>
+      </div>
+    </template>
   </div>
 </template>
-<script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
 import {
   selectCanvasCourseUrl,
   selectIsCanvasChime,
   selectJoinUrl,
-} from "../helpers/chimeSelectors";
-import toHyphenatedCode from "../helpers/toHyphenatedCode";
+} from "@/helpers/chimeSelectors";
+import toHyphenatedCode from "@/helpers/toHyphenatedCode";
+import QRCode from "@/components/QRCode.vue";
+import { Chime } from "@/types";
 
-export default {
-  props: {
-    chime: {
-      type: Object,
-      required: true,
-    },
-    includeFullUrl: {
-      type: Boolean,
-      require: false,
-    },
-  },
-  computed: {
-    isCanvasChime() {
-      return selectIsCanvasChime(this.chime);
-    },
-    canvasUrl() {
-      const fullCanvasUrlString = selectCanvasCourseUrl(this.chime);
-      return new URL(fullCanvasUrlString);
-    },
-    joinUrl() {
-      return selectJoinUrl(this.chime);
-    },
-    location() {
-      return window.location;
-    },
-  },
-  methods: {
-    toHyphenatedCode,
-  },
-};
+const props = defineProps<{
+  chime: Chime;
+}>();
+
+const isJoinForUngradedOpen = ref(false);
+
+const isCanvasChime = computed(() => selectIsCanvasChime(props.chime));
+
+const canvasUrl = computed(() => {
+  const fullCanvasUrlString = selectCanvasCourseUrl(props.chime);
+
+  if (!fullCanvasUrlString) return null;
+  return new URL(fullCanvasUrlString);
+});
+
+const joinUrl = computed(() => selectJoinUrl(props.chime));
+
+const { location } = window;
 </script>
 <style scoped>
 .join-panel {
---
+++ resources/assets/js/components/QRCode.vue
@@ -0,0 +1,18 @@
+<template>
+  <a :href="url" target="_blank" rel="noopener noreferrer">
+    <img :src="qrCodeSrc" alt="QR Code" class="tw-rounded" />
+  </a>
+</template>
+<script setup lang="ts">
+import { computedAsync } from "@vueuse/core";
+import { generateQRCode } from "@/helpers/qrCode";
+
+const props = defineProps<{
+  url: string;
+}>();
+
+const qrCodeSrc = computedAsync(async () => {
+  return generateQRCode(props.url);
+}, null);
+</script>
+<style scoped></style>
