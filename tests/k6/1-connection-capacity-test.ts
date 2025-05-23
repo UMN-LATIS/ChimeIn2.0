f676229f66726738944f783c7949c5643f10f620
James Johnson <johnsojr@umn.edu>
2025-05-23 18:02:46 -0500

    refactor

--- tests/k6/1-connection-capacity-test.ts
+++ tests/k6/1-connection-capacity-test.ts
@@ -5,7 +5,8 @@ import ws from "k6/ws";
 import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 
 const JOIN_URL = `http://localhost/join/52204`;
-const ECHO_SERVER_URL = `http://localhost:6001/socket.io/`;
+const ECHO_SERVER_URL_HTTP = `http://localhost:6001/socket.io/`;
+const ECHO_SERVER_URL_WS = `ws://localhost:6001/socket.io/`;
 
 export const options = {
   stages: [{ iterations: 1, target: 1 }],
@@ -16,8 +17,97 @@ export const options = {
     http_req_failed: ["rate<0.01"],
   },
   // Add explicit duration to ensure the test ends
-  duration: "15s",
+  // duration: "15s",
 };
+
+function getJoinPage() {
+  const res = http.get(JOIN_URL, {
+    redirects: 1,
+  });
+
+  const chimeIdStr = res.url.match(/\/chimeParticipant\/(\d+)/)?.[1];
+  const chimeId = chimeIdStr ? parseInt(chimeIdStr, 10) : null;
+
+  if (!chimeId) {
+    throw new Error("Chime ID not found in the response URL");
+  }
+
+  const csrfToken = res.cookies["XSRF-TOKEN"]?.[0]?.value;
+
+  check(res, {
+    "join page status is 200": (r) => r.status === 200,
+    "join page has cookies": (r) => Object.entries(r.cookies).length > 0,
+    "join page has chimeId": () => chimeId > 0,
+    "join page has csrfToken": () => !!csrfToken && csrfToken.length > 0,
+  });
+
+  return { res, chimeId, csrfToken };
+}
+
+function getEchoSession() {
+  const res = http.get(
+    `${ECHO_SERVER_URL_HTTP}?EIO=3&transport=polling&t=${crypto.randomUUID()}`,
+    {
+      redirects: 1,
+    }
+  );
+
+  if (typeof res.body !== "string") {
+    throw new Error("Socket.io response body is not a string");
+  }
+
+  const sidMatches = res.body?.match(/sid":"(.*?)"/);
+  const sid = sidMatches ? sidMatches[1] : null; // first match
+  const csrfToken = res.cookies["XSRF-TOKEN"]?.[0]?.value;
+
+  check(res, {
+    "socket.io page status is 200": (r) => r.status === 200,
+    "socket.io page has cookies": (r) => Object.entries(r.cookies).length > 0,
+    "socket.io page has sid": () => !!sid && sid.length > 0,
+    "socket.io page has csrfToken": () => !!csrfToken && csrfToken.length > 0,
+  });
+
+  return { sid, csrfToken };
+}
+
+function subscribeToChimePresenceChannel(
+  socketSend: (msg: string) => void,
+  chimeId: number,
+  csrfToken: string
+) {
+  const subscribeMessage = JSON.stringify([
+    "subscribe",
+    {
+      channel: `presence-session-status.${chimeId}`,
+      auth: {
+        headers: {
+          "X-CSRF-TOKEN": csrfToken,
+        },
+      },
+    },
+  ]);
+
+  // Send the subscription message
+  // socket.io uses 42 prefix to indicate a message
+  socketSend(`42${subscribeMessage}`);
+}
+
+function parseSocketIoMessage(data: string) {
+  // Check if the message starts with "42"
+  if (!data.startsWith("42")) {
+    return null;
+  }
+
+  // Extract the JSON part of the message
+  const jsonString = data.substring(2);
+  try {
+    // Parse the JSON string
+    const parsedMessage = JSON.parse(jsonString);
+    return parsedMessage;
+  } catch (e) {
+    console.error("Failed to parse JSON message:", e);
+  }
+}
 
 export default function () {
   console.log("=== TEST STARTING ===");
@@ -27,58 +117,13 @@ export default function () {
   const sleepDuration = totalSessionDuration / numSleeps / 1000; // seconds
 
   // 1: visit join page to get necessary cookies
-  const joinRes = http.get(JOIN_URL, {
-    redirects: 1,
-  });
+  const { res: joinRes, chimeId, csrfToken } = getJoinPage();
 
-  const chimeId = joinRes.url.match(/\/chimeParticipant\/(\d+)/)?.[1];
-
-  check(joinRes, {
-    "join page status is 200": (r) => r.status === 200,
-    "join page has cookies": (r) => Object.entries(r.cookies).length > 0,
-    "join page has chimeId": () => !!chimeId && chimeId.length > 0,
-  });
-
-  // 2. start handshake with Echo server
-  const socketIoRes = http.get(
-    `${ECHO_SERVER_URL}?EIO=3&transport=polling&t=${Date.now()}`
-  );
-
-  if (typeof socketIoRes.body !== "string") {
-    throw new Error("Socket.io response body is not a string");
-  }
-
-  const sidMatches = socketIoRes.body?.match(/sid":"(.*?)"/);
-  const sid = sidMatches ? sidMatches[1] : null; // first match
-
-  check(socketIoRes, {
-    "socket.io page status is 200": (r) => r.status === 200,
-    "socket.io page has cookies": (r) => Object.entries(r.cookies).length > 0,
-    "socket.io page has sid": () => !!sid && sid.length > 0,
-  });
-
-  console.log("Socket.IO SID:", sid);
+  // 2: get echo session to get sid and csrf token
+  const { sid } = getEchoSession();
 
   // connect with websockets
-  const wsUrl = `${ECHO_SERVER_URL.replace("http", "ws")}?EIO=3&transport=websocket&sid=${sid}`;
-
-  // Get CSRF token from cookies
-  let csrfToken = joinRes.cookies["XSRF-TOKEN"]?.[0]?.value;
-
-  // If the CSRF token is URL encoded, decode it
-  if (csrfToken) {
-    try {
-      csrfToken = decodeURIComponent(csrfToken);
-    } catch (e) {
-      console.error("Error decoding CSRF token:", e);
-    }
-  }
-
-  if (typeof csrfToken !== "string") {
-    throw new Error("CSRF token not found in the cookies");
-  }
-
-  console.log("CSRF Token:", csrfToken);
+  const wsUrl = `${ECHO_SERVER_URL_WS}?EIO=3&transport=websocket&sid=${sid}`;
 
   const wsParams = {
     headers: {
@@ -89,72 +134,40 @@ export default function () {
 
   const wsRes = ws.connect(wsUrl, wsParams, (socket) => {
     console.log("=== CONNECTING TO WEBSOCKET ===");
+    const socketSend = (msg: string) => {
+      socket.send(msg);
+      console.log("[client]", msg);
+    };
 
     socket.on("open", () => {
-      console.log("[server]: <open event received>");
+      console.log("=== WEBSOCKET OPEN ===");
 
-      // start handshake
-      socket.send("2probe");
-      console.log("[client]: 2probe");
+      // socket.io has a handshake protocol where it sends `2probe`
+      // and expects `3probe` in return
+      socketSend("2probe");
     });
 
     socket.on("message", (data) => {
-      // complete initial handshake
       if (data === "3probe") {
-        console.log("[server]: 3probe");
+        // after receiving 3probe, we send 5 to complete the handshake
+        socketSend("5");
 
-        socket.send("5");
-        console.log("[client]: 5");
+        console.log("=== WEBSOCKET HANDSHAKE COMPLETE ===");
+        subscribeToChimePresenceChannel(socketSend, chimeId, csrfToken);
 
-        // handshake complete
-        // subscribe to chime presence channel
-        const subscribeMessage = JSON.stringify([
-          "subscribe",
-          {
-            channel: `presence-session-status.${chimeId}`,
-            auth: {
-              headers: {
-                "X-CSRF-TOKEN": csrfToken,
-              },
-            },
-          },
-        ]);
-
-        // Send the subscription message
-        // socket.io uses 42 prefix to indicate a message
-        socket.send(`42${subscribeMessage}`);
-        console.log("[client]: 42", subscribeMessage);
-
-        // now set up ping/pong
-        socket.setInterval(
-          () => {
-            socket.send("2");
-            console.log("[client]: 2 (ping)");
-          },
-          randomIntBetween(1000, 2000)
-        );
+        // set up regular pings to the server
+        socket.setInterval(() => socketSend("2"), randomIntBetween(1000, 2000));
 
         return;
       }
 
       // Parse Socket.IO messages
       if (data.startsWith("42")) {
-        try {
-          const message = data.substring(2);
-          const parsedMessage = JSON.parse(message);
-          console.log("[server]", parsedMessage);
-        } catch (e) {
-          console.error("‼️ [error]", e);
-        }
+        console.log("[server]", parseSocketIoMessage(data));
         return;
       }
 
-      // Check for pong responses (server responds to our pings)
-      if (data === "3") {
-        console.log("[server] 3 (pong)");
-        return;
-      }
-
+      // log other messages
       console.log("[server]", data);
     });
 
@@ -168,7 +181,7 @@ export default function () {
 
     const SOCKET_TIMEOUT = totalSessionDuration + 2000;
     socket.setTimeout(function () {
-      console.log(`${SOCKET_TIMEOUT}ms passed, closing the socket`);
+      console.log(`=== CLOSING SOCKET ===`);
       socket.close();
     }, SOCKET_TIMEOUT);
   });
